2f3e2a3fac176aae69d7ad692f9c50338856d750
Dev Shukla <devshuklaji6@gmail.com>
2023-04-04 09:40:30 +0530

    added functionality

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
-import { AiFillDownCircle } from "react-icons/ai";
+import { AiFillDownCircle, AiFillDelete } from "react-icons/ai";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+// toast.configure();
 
 function Form() {
   const [clicked, setClicked] = useState(false);
   const handleClick = () => {
+    setClicked(!clicked);
+  };
+  const handleSave = () => {
+    toast("saved successfully");
+    setClicked(!clicked);
+  };
+  const handleDelete = () => {
+    toast("deleted successfully");
     setClicked(!clicked);
   };
   return clicked ? (
@@ -13,7 +24,16 @@ function Form() {
         <h1 className="heading">Project</h1>
         <AiFillDownCircle className="icon" onClick={handleClick} />
       </div>
-      <input type="text" className="input" placeholder="Project Name" />
+      <div className="main">
+        <div className="mainSection">
+          <input type="text" className="input" placeholder="Project Name" />
+          <AiFillDelete className="Delete" onClick={handleDelete} />
+        </div>
+        <button className="save" onClick={handleSave}>
+          Save
+        </button>
+        <ToastContainer />
+      </div>
     </div>
   ) : (
     <div className="container closedContainer">
@@ -21,6 +41,7 @@ function Form() {
         <h1 className="heading">Project</h1>
         <AiFillDownCircle className="icon" onClick={handleClick} />
       </div>
+      <ToastContainer />
     </div>
   );
 }
